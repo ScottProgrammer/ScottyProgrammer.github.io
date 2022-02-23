4ffd96662834966bbb33fa9bffa83566b859e3c4
ScottProgrammer <53801339+ScottProgrammer@users.noreply.github.com>
2022-02-23 20:28:24 +1100

    test

--- wwwroot/javascript/site.js
+++ wwwroot/javascript/site.js
@@ -2,7 +2,10 @@ fetch('~/navbar.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
+    if (oldelem != null)
+    {
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
+    }
 })
