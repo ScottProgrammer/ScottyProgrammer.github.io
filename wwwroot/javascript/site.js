31bae88f78d3f0c0a1ebc7fc837cb1674db01014
ScottProgrammer <53801339+ScottProgrammer@users.noreply.github.com>
2022-03-08 20:55:15 +1100

    more tests, added js

--- wwwroot/javascript/site.js
+++ wwwroot/javascript/site.js
@@ -27,7 +27,7 @@
 // allow collapsible to expand and retreact
 var coll = document.getElementsByClassName("collapsible");
 var i;
-
+console.log("testing" + coll.length);
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
