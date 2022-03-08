e0641ffa58db3abd2b66054019f235736cab1fd4
ScottProgrammer <53801339+ScottProgrammer@users.noreply.github.com>
2022-03-08 20:38:17 +1100

    added more pages

--- wwwroot/javascript/site.js
+++ wwwroot/javascript/site.js
@@ -22,3 +22,20 @@
         nav.
     }
 } */
+
+
+// allow collapsible to expand and retreact
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
