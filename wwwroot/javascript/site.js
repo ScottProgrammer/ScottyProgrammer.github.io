a1ea0b65155489e95388c23a110f55b3b9a5c221
ScottProgrammer <53801339+ScottProgrammer@users.noreply.github.com>
2022-03-16 20:34:41 +1100

    first attempt at swiping to change pages

--- wwwroot/javascript/site.js
+++ wwwroot/javascript/site.js
@@ -1,41 +1,84 @@
-/* fetch('navbar.html')
-.then(res => res.text())
-.then(text => {
-    // replace navbar div with navbar on page
-    let oldelem = document.querySelector("script#replace_with_navbar");
-    if (oldelem != null)
-    {
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-    }
-})
-
-// make current page link active in navbar
-  window.onload = function(e){ 
-    console.log("window.onload"); 
-    console.log(window.location.href);
-
-    var nav = document.getElementsByClassName('topnav');
-    if (nav != null)
-    {
-        nav.
-    }
-} */
-
-
 // allow collapsible to expand and retreact
 var coll = document.getElementsByClassName("collapsible");
 var i;
-console.log("testing" + coll.length);
+//console.log("testing" + coll.length);
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
   });
+}
+
+// Allow swiping to change pages
+// var col2 = document.getElementsByClassName("swipeable");
+// for (i = 0; i < col2.length; i++) {
+//   col2[i].addEventListener("ontouchstart", function(e) {
+//     e.preventDefault();
+//     console.log("touchstart");
+
+//   }, false);
+//   col2[i].addEventListener("ontouchend", function(e) {
+//     e.preventDefault();
+//     console.log("touchend");
+
+//   }, false);
+// }
+
+// Allow swiping to change pages
+var c_intTouchStartX;
+var c_intTouchStartY;
+var c_strPage;
+
+function touchStart(e, strPage){
+  c_intTouchStartX = e.touches[0].clientX;
+  c_intTouchStartY = e.touches[0].clientY;
+  c_strPage = strPage;
+  //console.log("touchStart " + strPage);
+}
+function touchMove(e){
+  
+  if (e.touches[0].clientY < c_intTouchStartY + 50 && e.touches[0].clientY > c_intTouchStartY - 50) // when scrolling up down don't accidently swap pages
+  {
+    if (e.touches[0].clientX < c_intTouchStartX - 150)
+    {
+      //console.log("swipe right");
+      switch (c_strPage)
+      {
+        case 'index':
+          window.location.href = "projects.html"
+          break;
+        case "projects":
+          window.location.href = "info.html"
+          break;
+        case "info":
+          window.location.href = "about.html"
+          break;
+      }
+    }
+    else if (e.touches[0].clientX > c_intTouchStartX + 150)
+    {
+        //console.log("swipe left");
+        switch (c_strPage)
+        {
+        case 'about':
+          window.location.href = "info.html"
+          break;
+        case "info":
+          window.location.href = "projects.html"
+          break;
+        case "projects":
+          window.location.href = "home.html"
+          break;
+      }
+    }
+  }
+}
+function touchEnd(e){
+  //console.log("touchend");
+  c_strPage = "";
 }
